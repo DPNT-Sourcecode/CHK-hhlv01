87f5f8f0f3e15cc63d33cf47cc60202e75717046
sbx_user1051 <sbx_user1051@169.254.107.81>
2023-11-06 12:12:24 +0000

    Mon Nov 06 12:12:24 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,8 +11,9 @@ def checkout(skus):
         sku_items = checkout_service.create_skus(skus)
 
         if sku_items != -1:
-            cost = checkout_service.calculate_cost()
+            cost = checkout_service.calculate_cost(sku_items)
             return cost
 
     return -1  # if the input is None
+
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -13,15 +13,23 @@ class TestCheckout:
         sku_items = checkout_service.create_skus(data)
         assert expected == checkout_service.calculate_cost(sku_items)
 
-    @pytest.mark.parametrize("data,expected", [("A", 50), ("ABCD", 115), ("CCCC", 80)])
+    @pytest.mark.parametrize(
+        "data,expected", [("AAAA", 180), ("BB", 45), ("BBBBAAA", 220)]
+    )
     def test_checkout_cost_offer(self, data, expected, checkout_service):
         sku_items = checkout_service.create_skus(data)
         assert expected == checkout_service.calculate_cost(sku_items)
 
     @pytest.mark.parametrize("data,expected", [("A1", -1), ("", -1), ("Z", -1)])
-    def test_checkout_cost_invalid(self, data, expected, checkout_service):
+    def test_checkout_create(self, data, expected, checkout_service):
         sku_items = checkout_service.create_skus(data)
-        assert expected == checkout_service.calculate_cost(sku_items)
+        assert expected == sku_items
+
+    @pytest.mark.parametrize("data,expected", [("A1", -1), ("", -1), ("Z", -1)])
+    def test_checkout_create_invalid(self, data, expected, checkout_service):
+        sku_items = checkout_service.create_skus(data)
+        assert expected == sku_items
+
 
 
 
