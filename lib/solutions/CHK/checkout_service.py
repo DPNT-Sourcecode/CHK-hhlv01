8f133be9c71d8b421e0966627ec94be5ffbeac21
sbx_user1051 <sbx_user1051@169.254.61.189>
2023-11-06 10:58:30 +0000

    Mon Nov 06 10:58:30 UTC 2023

--- lib/solutions/CHK/checkout_service.py
+++ lib/solutions/CHK/checkout_service.py
@@ -1,5 +1,18 @@
+from typing import Iterable, Union
+
+from lib.solutions.CHK.models import SKUItem
+
+
 class CheckoutService:
-    def create_sku_items(self, skus: str):
+    SKU_PRICES = {"A": 50, "B": 30, "C": 20, "D": 15}
+
+    def create_sku_items(self, skus: str) -> Union[Iterable[SKUItem, int]]:
+        """
+        Return a list of SKUItems or -1 if any items are invalid
+        """
         skus_list = list(skus)
 
+        [for sku in skus_list]
+
         return
+
