0a18c5f68ff67c8a9a5f9ce55ff51df937d35009
sbx_user1051 <sbx_user1051@169.254.61.189>
2023-11-06 11:01:30 +0000

    Mon Nov 06 11:01:30 UTC 2023

--- lib/solutions/CHK/checkout_service.py
+++ lib/solutions/CHK/checkout_service.py
@@ -6,13 +6,22 @@ from lib.solutions.CHK.models import SKUItem
 class CheckoutService:
     SKU_PRICES = {"A": 50, "B": 30, "C": 20, "D": 15}
 
-    def create_sku_items(self, skus: str) -> Union[Iterable[SKUItem, int]]:
+    @staticmethod
+    def _validate_sku(sku: str) -> bool:
+        """
+        Return True if the sku is valid, False otherwise
+        """
+        return False # TODO:
+
+    @staticmethod
+    def create_sku_items(skus: str) -> Union[Iterable[SKUItem, int]]:
         """
         Return a list of SKUItems or -1 if any items are invalid
         """
         skus_list = list(skus)
 
-        [for sku in skus_list]
+        [SKUItem(sku, SKU_PRICES.get()) for sku in skus_list if ]
 
         return
+
 
