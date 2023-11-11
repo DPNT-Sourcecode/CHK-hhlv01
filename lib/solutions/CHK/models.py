4b79a0aea27bf7e81ecd4b1e38b2a546f02a4a34
sbx_user1051 <sbx_user1051@169.254.54.77>
2023-11-11 17:23:05 +0000

    Sat Nov 11 17:23:05 UTC 2023

--- lib/solutions/CHK/models.py
+++ lib/solutions/CHK/models.py
@@ -12,10 +12,17 @@ class SKUItem:
         self.offer_applied = True
         return self
 
+
 @dataclasses.dataclass
 class Condition:
     sku: str
     quantity: int
+
+    def applies(self, skus: Dict[str, Iterable[SKUItem]]):
+        valid_skus = skus.get(self.sku, [])
+        return len(valid_skus) >= self.quantity and all(
+            not sku.offer_applied for sku in valid_skus
+        )
 
 
 @dataclasses.dataclass
@@ -33,21 +40,24 @@ class Offer:
     def apply(self, skus: Dict[str, Iterable[SKUItem]]):
         # if condition is valid and item not offer_applied
 
-        valid_skus = skus.get(self.condition.sku, [])
-        if len(valid_skus) >= self.condition.quantity and all(not sku.offer_applied for sku in valid_skus):
 
-
+        if len(valid_skus) >= self.condition.quantity and all(
+            not sku.offer_applied for sku in valid_skus
+        ):
             skus[self.condition.sku] = [sku.set_offer_applied() for sku in valid_skus]
 
             # apply result
-            self.result.
-            skus[self.result.sku] = update_sku
+            res_skus = skus.get(self.result.sku, [])
 
+            result.
+
+            skus[self.result.sku] = res_skus
 
             # call apply again until offer cond unsatisified
             skus = self.apply(skus)
 
         return skus
+
 
 
 
