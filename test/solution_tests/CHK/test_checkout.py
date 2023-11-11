d4b6217537169d7b973fdb4122d46e8e43b626a8
sbx_user1051 <sbx_user1051@169.254.29.137>
2023-11-11 21:30:08 +0000

    Sat Nov 11 21:30:08 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -17,8 +17,22 @@ class TestCheckoutService:
         sku_items = checkout_service.create_skus(data)
         assert expected == checkout_service.calculate_cost(sku_items)
 
-    # ("AAAA", 180), ("BB", 45), ("BBBBAAA", 220),
-    @pytest.mark.parametrize("data,expected", [("FFF", 20)])
+    @pytest.mark.parametrize(
+        "data,expected",
+        [
+            ("FFF", 20),
+            ("AAAA", 180),
+            ("BB", 45),
+            ("BBBBAAA", 220),
+            ("PPPPP", 200),
+            ("KK", 150),
+            ("HHHHH", 45),
+            ("HHHHHHHHHH", 80),
+            ("QQQ", 80),
+            ("VV", 90),
+            ("VVV", 130),
+        ],
+    )
     def test_checkout_cost_single_offer(self, data, expected, checkout_service):
         sku_items = checkout_service.create_skus(data)
         assert expected == checkout_service.calculate_cost(sku_items)
@@ -30,6 +44,9 @@ class TestCheckoutService:
             ("CCADDEEBBA", 280),
             ("AAAAAEEBAAABB", 455),
             ("EEEEBB", 160),
+            ("NNNM", 120),
+            ("RRRQ", 150),
+            ("UUUU", 120),
         ],
     )
     def test_checkout_cost_diff_offer(self, data, expected, checkout_service):
@@ -53,6 +70,7 @@ class TestCheckout:
     @pytest.mark.parametrize("data,expected", [("", 0)])
     def test_checkout(self, data, expected):
         assert checkout(data) == expected
+
 
 
 
